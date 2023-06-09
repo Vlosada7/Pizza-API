86bd0f406a3cbd0a7a6aec68ac096236dc318571
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-06-09 16:48:53 +0200

    newOrder controller

--- src/controllers/odererControllers.ts
+++ src/controllers/odererControllers.ts
@@ -35,8 +35,89 @@ const getOrdererId = async (req: Request, res: Response) => {
 };
 
 const newOrderer = async (req: Request, res: Response) => {
-	res.status(202).send("New orderer check");
+  try {
+		const { pizzaId, pizzaQuantity } = req.body;
+		if (!pizzaId || !pizzaQuantity) {
+			res.status(400).send("Missing pizzaId or pizzaQuantity");
+			return;
+		}
+
+		const orderes = await prisma.order.findMany();
+		if (orderes.length === 0) {
+			const salesman = await prisma.salesman.findFirst({
+				where: {
+					round_robin_index: 1,
+				},
+			});
+
+			if (!salesman) {
+				res.status(404).send("No salesman in database");
+				return;
+			}
+
+			const newOrderer = await prisma.order.create({
+				data: {
+					salesmanId: salesman.id,
+				},
+			});
+
+			const newOrdererItem = await prisma.orderItem.create({
+				data: {
+					pizzaId,
+					orderId: newOrderer.id,
+					quantity: pizzaQuantity,
+				},
+			});
+
+			res.status(201).send(newOrdererItem);
+		} else {
+			const lastOrder = orderes[orderes.length - 1];
+			const lastSalesmanId = lastOrder.salesmanId;
+			const lastSalesman = await prisma.salesman.findUnique({
+				where: {
+					id: lastSalesmanId,
+				},
+			});
+
+			if (!lastSalesman) {
+				res.status(404).send("No salesman in database");
+				return;
+			}
+
+			const nextSalesmanIndex = lastSalesman.round_robin_index + 1;
+			const nextSalesman = await prisma.salesman.findFirst({
+				where: {
+					round_robin_index: nextSalesmanIndex,
+				},
+			});
+
+			if (!nextSalesman) {
+				res.status(404).send("No salesman in database");
+				return;
+			}
+
+			const newOrderer = await prisma.order.create({
+				data: {
+					salesmanId: nextSalesman.id,
+				},
+			});
+
+			const newOrdererItem = await prisma.orderItem.create({
+				data: {
+					pizzaId,
+					orderId: newOrderer.id,
+					quantity: pizzaQuantity,
+				},
+			});
+
+			res.status(201).send(newOrdererItem);
+		}
+	} catch (error) {
+		console.error("Error in newOrderer:", error);
+		res.status(500).send("Internal Server Error");
+	}
 };
+
 
 const thankYouEmail = async (req: Request, res: Response) => {
 	res.status(202).send("Send email check");
